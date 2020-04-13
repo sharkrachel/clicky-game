8c1933b81ce6f3bb380ed6ec6649fc6a6c6d58cb
Rachel Sipes <rasipes@gmail.com>
2020-04-12 21:38:52 -0400

    created function for handling click, wining, losing

--- client/src/App.js
+++ client/src/App.js
@@ -15,47 +15,84 @@ class App extends Component {
       score: 0,
       clickedCharacters: []
     }
-    this.handleClick=this.handleClick.bind(this)
+    this.handleClick=this.handleClick.bind(this);
   }
 
-  handleClick() {
-    console.log("clicked");
+  handleClick(character) {
+    this.checkWinLose(character);
+
   }
 
   shuffleCards() {
 
   }
 
-  scoreTally() {
-
+  incrementScore() {
+    this.setState({ score: this.state.score + 1 });
+    console.log("increment score")
   }
 
-  checkWinLose() {
+  checkWinLose(character) {
+    const { id } = character;
+    const isAlreadyClicked = this.state.clickedCharacters.find(storedId => {
+      console.log("stored: ", storedId);
+      console.log("id: ", id);
+      return storedId === id;
 
+
+    });
+    console.log("Already clicked?: ", isAlreadyClicked)
+    if (isAlreadyClicked) {
+      this.gameLose();
+    }
+    else if(this.state.score === 11) {
+      this.incrementScore();
+      this.gameWon();
+    }
+    else {
+      let { clickedCharacters } = this.state;
+      clickedCharacters = [].concat(clickedCharacters);
+      clickedCharacters.push(id);
+      this.setState({ clickedCharacters });
+      this.incrementScore();
+      this.shuffleCards();
+      
+    }
+    console.log("clicked characters check win: ", this.state.clickedCharacters)
   }
 
   resetGame() {
-
+    this.setState({ score: 0 });
   }
 
   gameWon() {
 
+    this.resetGame();
+
   }
 
-
+  gameLose() {
+    this.resetGame();
+  }
 
   render() {
     return (
       <Container>
+        <div>
+          { this.state.score }
+        </div>
         <Row>
-          {this.state.characters.map(character => {
+          { this.state.characters.map(character => {
             return (
               <Column key={character.id} column="col-sm-3">
 
                 <CharacterCard
                   image={character.image}
 
-                  onClick={this.handleClick}
+                  onClick={ () => {
+                    this.handleClick (character)
+                  }
+                }
                 />
               </Column>
             )
