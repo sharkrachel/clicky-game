b85557925aa3d3a1c0666158bc047e7473e75acc
Rachel Sipes <rasipes@gmail.com>
2020-04-18 12:11:58 -0400

    Updates

---
+++ precache-manifest.00f14767a473bc767be7e4104f705185.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28981d97a5960c090cfa8151fee0453d",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "24f2b7dd268ad350838c",
+    "url": "/clicky-game/static/css/main.bf1b1d50.chunk.css"
+  },
+  {
+    "revision": "e70cf8aac18ceb6ec8af",
+    "url": "/clicky-game/static/js/2.1df45196.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.1df45196.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24f2b7dd268ad350838c",
+    "url": "/clicky-game/static/js/main.05f02566.chunk.js"
+  },
+  {
+    "revision": "823b3864c12148873107",
+    "url": "/clicky-game/static/js/runtime-main.59b16ebf.js"
+  }
+]);
